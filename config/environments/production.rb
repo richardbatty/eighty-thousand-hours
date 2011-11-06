d9288e40a8705d9b8320dc1ad728a58e92156996
Matt Gibb <matt@mattgibb.com>
2011-11-06 19:09:53 +0000

    Outbound email config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,11 @@ HighImpactCareers::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
+  
   # outbound email config
   config.action_mailer.default_url_options = { :host => 'highimpactcareers.org' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 end
