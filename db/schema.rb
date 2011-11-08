03b1fcbe45be583a7574381822ad853aa175f930
Matt Gibb <matt@mattgibb.com>
2011-11-08 10:01:22 +0000

    User seed data.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111104195526) do
+ActiveRecord::Schema.define(:version => 20111107181840) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(:version => 20111104195526) do
     t.string   "authentication_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.text     "bio"
+    t.boolean  "pledge"
+    t.boolean  "public"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! name: 'Campaigning Catherine',
+                    email: 'campaigning.catherine@example.com',
+                    password:              'know_your_enemy',
+                    password_confirmation: 'know_your_enemy'
+puts 'New user created: ' << user.name
