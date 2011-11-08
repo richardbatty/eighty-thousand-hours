dce8865a3525d99b54d8dc10d66f0f1b20db9b00
Matt Gibb <matt@mattgibb.com>
2011-11-08 07:48:33 +0000

    No need to configure outgoing mail server in development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ HighImpactCareers::Application.configure do
   # A dummy setup for development - no deliveries, but logged
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = false
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   
 end
