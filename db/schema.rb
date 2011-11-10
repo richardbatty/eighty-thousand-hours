810d16e523f9ff87d6d5d3f4550aefbc3dd8f848
Folcon Red <folcon@gmail.com>
2011-11-10 21:32:55 +0000

    Just generated scaffolding for posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111107181840) do
+ActiveRecord::Schema.define(:version => 20111110205538) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "draft"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
