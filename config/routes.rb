0455dbcebbb5cbdce2cb52873270bdd6ede36606
rjshade <robbie.shade@gmail.com>
2011-11-10 14:00:39 +0000

    spacing

--- config/routes.rb
+++ config/routes.rb
@@ -49,11 +49,12 @@ HighImpactCareers::Application.routes.draw do
   #   end
   
   root :to => 'info#index'
-  match 'ethical-career' => 'info#ethical_career'
-  match 'what-you-can-do' => 'info#what_you_can_do'
-  match 'what-you-can-do/my-donations' => 'info#my_donations'
-  match 'what-you-can-do/my-career' => 'info#my_career'
-  match 'what-we-do' => 'info#what_we_do'
+  match 'ethical-career'                => 'info#ethical_career'
+  match 'what-you-can-do'               => 'info#what_you_can_do'
+  match 'what-you-can-do/my-donations'  => 'info#my_donations'
+  match 'what-you-can-do/my-career'     => 'info#my_career'
+  match 'what-we-do'                    => 'info#what_we_do'
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
