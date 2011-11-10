77c52ab9b260110287bf9007adf9e66e40a411f4
Matt Gibb <matt@mattgibb.com>
2011-11-10 20:08:45 +0000

    Removed superfluous users routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 HighImpactCareers::Application.routes.draw do
   devise_for :users
   
-  resources :users, :only => [:index, :show, :edit, :update]
+  resources :users, :only => [:index, :show]
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
