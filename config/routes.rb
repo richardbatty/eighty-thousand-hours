2578d06a23c181c0b2e0d1bef5eca80c84ef5ec2
rjshade <robbie.shade@gmail.com>
2011-11-10 15:15:28 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,8 @@ HighImpactCareers::Application.routes.draw do
   match 'what-we-do'                    => 'info#what_we_do'
   match 'get-involved'                  => 'info#get_involved'
   match 'volunteer'                     => 'info#volunteer'
-  match 'FAQ'                           => 'info#FAQ'
+  match 'faq'                           => 'info#faq'
+  match 'get-involved/join'             => 'info#join'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
