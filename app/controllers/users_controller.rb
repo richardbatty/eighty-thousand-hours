45f38efe9fb4fe22aadd0fa0179792ea20ef9f90
rjshade <robbie.shade@gmail.com>
2011-11-18 18:22:58 +0000

    TODO msg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,12 @@
 class UsersController < ApplicationController
+
+  #TODO this should only be accessible by Admins
+
   def index
     @users = User.all
   end
 
   def show
     @user = User.find_by_id( params[:id] )
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
   end
 end
