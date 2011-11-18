3c343b6b30ae27e12430692c7d582ad2a91b3a49
rjshade <robbie.shade@gmail.com>
2011-11-18 18:12:12 +0000

    creating new users and logging in/out

--- db/migrate/20111118125639_create_profiles.rb
+++ db/migrate/20111118125639_create_profiles.rb
@@ -1,14 +1,36 @@
 class CreateProfiles < ActiveRecord::Migration
   def change
     create_table :profiles do |t|
-      t.string  :background
-      t.string  :career_plans
-      t.string  :location
-      t.boolean :pledge
 
+      # who are you, condensed life story
+      t.string :background
+
+      # future plans, justify how this best achieves aim of doing most good
+      t.string :career_plans
+
+      # what inspired you to become a member?
+      t.string :inspiration
+
+      # add some color e.g. "I once ran to Mongolia for SCI"
+      t.string :interesting_fact
+
+      # physical location e.g. Oxford, England
+      t.string :location
+
+      # your role in the organisation e.g. volunteer blogger
+      t.string :organisation_role
+
+      # can we list your name on the website?
+      t.boolean :show_name, :default => true
+
+      # should we display your info on the member profile page?
+      t.boolean :show_info, :default => true
+
+      # a Profile belongs_to a User
       t.references :user
 
       t.timestamps
+
     end
   end
 end
