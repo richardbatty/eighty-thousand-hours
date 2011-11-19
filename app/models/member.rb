fcfd46cd3cdd9e883d7d988b313f9c6d02597fc3
rjshade <robbie.shade@gmail.com>
2011-11-19 12:58:02 +0000

    reorg of partials and helpers

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,9 @@
-class Member
-  def new( profile_params, user_params )
-    @m
-  end
+class Profile < ActiveRecord::Base
+  attr_accessible :background, :career_plans, :location, :user
+
+  validates_presence_of :background, :career_plans
+
+  # a Profile is always tied to a User 
+  belongs_to :user
 end
+
--- app/models/profile.rb
+++
@@ -1,9 +0,0 @@
-class Profile < ActiveRecord::Base
-  attr_accessible :background, :career_plans, :location, :user
-
-  validates_presence_of :background, :career_plans
-
-  # a Profile is always tied to a User 
-  belongs_to :user
-end
-
