c2f48f6dcd836f890c9f2faefb0117ff38ef98fe
rjshade <robbie.shade@gmail.com>
2011-11-19 22:13:21 +0000

    button links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def org_name
     "80,000 Hours"
   end
+
+  def button_link( text, path, klass = nil )
+    content_tag( :div, :class => "center" ) do
+      link_to  text, path, :class => (klass ? "button " + klass : "button")
+    end
+  end
 end
