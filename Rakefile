3a2704bed88a1169a3f2b479f389fddbcd8e4184
rjshade <robbie.shade@gmail.com>
2011-11-20 17:48:40 +0000

    secrets now live outside of repo

--- Rakefile
+++ Rakefile
@@ -5,3 +5,16 @@
 require File.expand_path('../config/application', __FILE__)
 
 EightyThousandHours::Application.load_tasks
+
+secret_path = '/Users/rjs/.80000hours_secrets.rb'
+require secret_path if File.exists?( secret_path ) 
+
+task :run => [:set_development_environment] do
+    sh "rails server"
+end
+
+task :push_to_heroku do
+  sh "git push heroku"
+  :set_heroku_config_vars
+end
+
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-EightyThousandHours::Application.config.secret_token = 'b6569dafe208bb1c9af9a2f8791b7878b22a13a394144871f1aa7682975edac48c7e8baa02dbe1b8a905b17f6686fdf581cc3df5ccda420f4986e9d974ea9c13'
+EightyThousandHours::Application.config.secret_token = ENV['COOKIE_SECRET']
