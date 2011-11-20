10697e3eef7b66d13ee760e5ee2c92f10fbb1c24
rjshade <robbie.shade@gmail.com>
2011-11-20 16:41:27 +0000

    scheme updated for confirmed member

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20111118133108) do
     t.string   "organisation_role"
     t.boolean  "show_name",         :default => true
     t.boolean  "show_info",         :default => true
+    t.boolean  "confirmed",         :default => false
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
