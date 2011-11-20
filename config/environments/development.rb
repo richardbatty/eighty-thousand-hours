7446e5253eb1f0a201f713ca0e645770edd0ac10
rjshade <robbie.shade@gmail.com>
2011-11-20 20:01:13 +0000

    development email environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,17 +33,9 @@ EightyThousandHours::Application.configure do
 
   # A dummy setup for development - no deliveries, but logged
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.smtp_settings = {
-    :address              => ENV['SMTP_ADDRESS'],
-    :port                 => ENV['SMTP_PORT'],
-    :domain               => ENV['SMTP_DOMAIN'],
-    :user_name            => ENV['SMTP_USERNAME'],
-    :password             => ENV['SMTP_PASSWORD'],
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
 
   # only used in development -- real secret hidden in external file
   if ENV['COOKIE_SECRET'].nil?
