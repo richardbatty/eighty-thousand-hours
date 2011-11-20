7e6231d7ca93fb97b079720e22e84a61ea0dac8c
rjshade <robbie.shade@gmail.com>
2011-11-20 18:47:31 +0000

    smtp settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ EightyThousandHours::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -30,12 +30,20 @@ EightyThousandHours::Application.configure do
   
   # outbound email config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # A dummy setup for development - no deliveries, but logged
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = false
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
-  
+  config.action_mailer.smtp_settings = {
+    :address              => ENV['SMTP_ADDRESS'],
+    :port                 => ENV['SMTP_PORT'],
+    :domain               => ENV['SMTP_DOMAIN'],
+    :user_name            => ENV['SMTP_USERNAME'],
+    :password             => ENV['SMTP_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
 
 silence_warnings do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ EightyThousandHours::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
@@ -62,8 +62,17 @@ EightyThousandHours::Application.configure do
   
   # outbound email config
   config.action_mailer.default_url_options = { :host => 'eightythousand.org' }
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => ENV['SMTP_ADDRESS'],
+    :port                 => ENV['SMTP_PORT'],
+    :domain               => ENV['SMTP_DOMAIN'],
+    :user_name            => ENV['SMTP_USERNAME'],
+    :password             => ENV['SMTP_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
