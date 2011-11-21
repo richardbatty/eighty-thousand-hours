4a839cf0b55a44a304a6df5a04e99a513fec4778
rjshade <robbie.shade@gmail.com>
2011-11-21 15:30:53 +0000

    added avatar fields to member model

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,15 @@ class Member < ActiveRecord::Base
 
   validates_presence_of :background, :career_plans
 
+  # paperclip avatars on S3
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :storage => :s3,
+                    :s3_credentials => { :access_key_id     => ENV['S3_ACCESS'],
+                                         :secret_access_key => ENV['S3_SECRET'] },
+                    :path => ":attachment/:id/:style.:extension",
+                    :bucket => 'eighty-thousand-hours',
+                    :default_url => '/images/missing_:style.jpg'
+
   # a Member is always tied to a User 
   belongs_to :user
 
