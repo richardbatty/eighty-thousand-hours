39745c5b1e1e89d98ad272f8766fddc06652be32
Matt Gibb <matt@mattgibb.com>
2011-11-22 19:24:25 +0000

    Fixed confirmed shuffle.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   #TODO all of these should only be accessible by Admins
   def index
-    @users = User.with_member
+    @users = User.with_member.confirmed.shuffle
   end
 
   def show
