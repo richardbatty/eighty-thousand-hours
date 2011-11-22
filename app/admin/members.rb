ebdd6782a5a60a9c615c59cdc0aeab44af21f2d8
Matt Gibb <matt@mattgibb.com>
2011-11-22 10:50:05 +0000

    Specified admin user form.

--- app/admin/members.rb
+++ app/admin/members.rb
@@ -1,3 +1,18 @@
 ActiveAdmin.register Member do
-  
+  form do |f|
+    f.inputs "Details" do
+      f.inputs :user,
+               :background,
+               :career_plans,
+               :inspiration,
+               :interesting_fact,
+               :location,
+               :organisation_role,
+               :show_name,
+               :show_info,
+               :confirmed,
+               :avatar
+      f.buttons
+    end
+  end
 end
