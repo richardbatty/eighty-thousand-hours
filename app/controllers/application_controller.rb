6197906fba49c9a9481318769a87ecca0c7a8e4a
Matt Gibb <matt@mattgibb.com>
2011-11-22 01:13:28 +0000

    Fixed admin authentication and authorisation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+  
+  def authorize_admin!
+    authenticate_user!
+    raise CanCan::AccessDenied unless current_user.has_role? :admin
+  end
 end
