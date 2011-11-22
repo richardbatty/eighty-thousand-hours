fe69dca37920332dd2afc57004b641a2047cf2b0
rjshade <robbie.shade@gmail.com>
2011-11-22 11:21:46 +0000

    temp disable email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :member_attributes
 
   validates_presence_of :name
-  validates_uniqueness_of :name, :email, case_sensitive: false
+  #validates_uniqueness_of :name, :email, case_sensitive: false
+  validates_uniqueness_of :name, case_sensitive: false
   validates_confirmation_of :password
 
   has_and_belongs_to_many :roles
