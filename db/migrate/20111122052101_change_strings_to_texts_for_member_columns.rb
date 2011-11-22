9f08b5e6ad3cccd2666ce3bed7e3a5274d35d775
Matt Gibb <matt@mattgibb.com>
2011-11-22 13:06:25 +0000

    Neatened migration.

--- db/migrate/20111122052101_change_strings_to_texts_for_member_columns.rb
+++ db/migrate/20111122052101_change_strings_to_texts_for_member_columns.rb
@@ -1,21 +1,17 @@
 class ChangeStringsToTextsForMemberColumns < ActiveRecord::Migration
-  def self.up
-    change_table :members do |t|
-      t.change :background, :text
-      t.change :career_plans, :text
-      t.change :inspiration, :text
-      t.change :interesting_fact, :text
-      t.change :organisation_role, :text
-     end
+  def up
+    change_column :members, :background, :text
+    change_column :members, :career_plans, :text
+    change_column :members, :inspiration, :text
+    change_column :members, :interesting_fact, :text
+    change_column :members, :organisation_role, :text
   end
 
-  def self.down
-    change_table :members do |t|
-      t.change :background, :string
-      t.change :career_plans, :string
-      t.change :inspiration, :string
-      t.change :interesting_fact, :string
-      t.change :organisation_role, :string
-     end
+  def down
+    change_column :members, :background, :string
+    change_column :members, :career_plans, :string
+    change_column :members, :inspiration, :string
+    change_column :members, :interesting_fact, :string
+    change_column :members, :organisation_role, :string
   end
 end
