62af68afdf18c1a5d54ed6ed9d11844a2c7de34d
Matt Gibb <matt@mattgibb.com>
2011-11-23 20:36:46 +0000

    Posts admin interface.

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,4 +1,16 @@
 ActiveAdmin.register Post do
   scope :draft
   scope :published
+  
+  form do |f|
+    f.inputs "Details" do
+      f.inputs :title,
+               :body,
+               :author,
+               :created_at,
+               :draft
+      f.buttons
+    end
+  end
+  
 end
