f4f04f482cf426a96aa19d019b190ea5ec74a8f5
Matt Gibb <matt@mattgibb.com>
2011-11-23 23:09:10 +0000

    Added privacy fields to signup.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,8 @@
 class Member < ActiveRecord::Base
   attr_accessible :background, :career_plans, :location,
                   :confirmed, :avatar, :inspiration, :interesting_fact,
-                  :location, :organisation_role, :phone, :pledge
+                  :location, :organisation_role, :phone, :pledge,
+                  :show_name, :show_info
 
   # paperclip avatars on S3
   has_attached_file :avatar, 
