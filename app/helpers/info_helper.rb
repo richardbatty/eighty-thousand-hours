0021e6865a970e12d23591373950c4656c48bf26
rjshade <robbie.shade@gmail.com>
2011-11-24 17:41:17 +0000

    updated page title

--- app/helpers/info_helper.rb
+++ app/helpers/info_helper.rb
@@ -1,7 +1,7 @@
 module InfoHelper
   def page_title
     content_tag :title do
-      "Eighty Thousand Hours" + ( @title ? " - #{@title}" : "" )
+      "80,000 Hours" + ( @title ? " - #{@title}" : "" )
     end
   end
 end
