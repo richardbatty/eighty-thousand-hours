b07bdcd040c1e4bdf7d2e4b35270b4b89f14bd91
rjshade <robbie.shade@gmail.com>
2011-11-24 12:49:36 +0000

    slugs for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :title, :use => :slugged
+
   scope :draft,     where(:draft => true).order("created_at DESC")
   scope :published, where(:draft => false).order("created_at DESC")
+
+  # override to_param to specify format of URL
+  # now we can call post_path(@post) and get
+  # "/blog/8-today-show" returned for example
+  def to_param
+    "#{self.id}-#{self.friendly_id}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111123200019) do
+ActiveRecord::Schema.define(:version => 20111124123444) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -55,7 +55,10 @@ ActiveRecord::Schema.define(:version => 20111123200019) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "author"
+    t.string   "slug"
   end
+
+  add_index "posts", ["slug"], :name => "index_posts_on_slug", :unique => true
 
   create_table "roles", :force => true do |t|
     t.string   "name"
