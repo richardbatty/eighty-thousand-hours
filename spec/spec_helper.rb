db883350e1b3fd26e0332aac35bd362bcc5c2b66
Matt Gibb <matt@mattgibb.com>
2011-12-20 15:12:38 +0000

    Fixed retarded default environment settings.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,6 +31,7 @@ require 'rubygems'
 require 'spork'
 
 Spork.prefork do
+  ENV["RAILS_ENV"] = 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
