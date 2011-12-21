4a4ccd92601ea72fb864023a90bf8742797b993b
rjshade <robbie.shade@gmail.com>
2011-12-21 22:43:35 +0000

    Changed # of posts on blog index from 3->6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.published.paginate(:page => params[:page], :per_page => 3)
+    @posts = Post.published.paginate(:page => params[:page], :per_page => 6)
   end
 
   def show
