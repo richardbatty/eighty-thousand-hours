b3388a89924436c98768eba22f0d4d8f7d5b7474
Matt Gibb <matt@mattgibb.com>
2011-12-21 11:06:07 +0000

    Fixed internationalisation.

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,7 @@ module EightyThousandHours
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :'en-GB'
+    config.i18n.locale = :'en-GB'
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
