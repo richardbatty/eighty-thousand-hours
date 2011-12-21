a6f5353fc3544d67e4112933443624ab132c15b9
Matt Gibb <matt@mattgibb.com>
2011-12-21 11:34:47 +0000

    Email is validated by confirmation.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,22 +11,6 @@ describe User do
     no_email_user.should_not be_valid
   end
   
-  it "should accept valid email addresses" do
-    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
-    addresses.each do |address|
-      valid_email_user = FactoryGirl.build :user, email: address
-      valid_email_user.should be_valid
-    end
-  end
-  
-  it "should reject invalid email addresses" do
-    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
-    addresses.each do |address|
-      invalid_email_user = FactoryGirl.build :user, email: address
-      invalid_email_user.should_not be_valid
-    end
-  end
-  
   it "should reject duplicate email addresses" do
     user = FactoryGirl.create :user
     user_with_duplicate_email = FactoryGirl.build :user, email: user.email
