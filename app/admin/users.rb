2eb3cab96ca9c422ba3292e13e4726bb52346cfb
rjshade <robbie.shade@gmail.com>
2012-02-10 21:32:44 -0500

    tidied up user admin tab

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,11 @@
 ActiveAdmin.register User do
   menu :if => proc{ can?(:manage, User) }
   controller.authorize_resource
+
+  index do
+    column :id
+    column :name
+    column :slug
+    column :last_sign_in_at
+  end
 end
