b9474e74db192adf2db23414c6db0fc6ee8d1937
rjshade <robbie.shade@gmail.com>
2012-02-16 14:21:48 -0500

    only display existing users

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -5,7 +5,7 @@ ActiveAdmin::Dashboards.build do
       column "Title" do |v| v.item.respond_to?('title') ? v.item.title : v.item.name end
       column "Type" do |v| v.item_type.underscore.humanize end
       column "Modified at" do |v| v.created_at.to_s :long end
-      column "Edited by" do |v| link_to User.find(v.whodunnit).name, admin_user_path(User.find(v.whodunnit)) end
+      column "Edited by" do |v| (link_to User.find(v.whodunnit).name, admin_user_path(User.find(v.whodunnit))) if v.whodunnit end
     end
   end
 end
