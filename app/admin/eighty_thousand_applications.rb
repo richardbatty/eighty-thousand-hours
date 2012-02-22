88181d85699b82859f3b89bfc9b40e93c3985d1a
rjshade <robbie.shade@gmail.com>
2012-02-21 22:02:40 -0500

    slight improvement to application admin

--- app/admin/eighty_thousand_applications.rb
+++ app/admin/eighty_thousand_applications.rb
@@ -5,4 +5,20 @@ ActiveAdmin.register EightyThousandApplication do
   action_item :only => :show do
     link_to "Confirm member", confirm_admin_member_path(eighty_thousand_application.member), :method => :put
   end
+
+  index do
+    column :id
+    column :name do |e|
+      e.member.name
+    end
+    column :occupation
+    column :career_plans
+    column :reasons_for_joining
+
+    column "Confirmed?" do |e|
+      e.member.confirmed ? "<span class='status ok'>YES</span>".html_safe : "<span class='status error'>No</span> (#{link_to "Confirm", confirm_admin_member_path(e.member), :method => :put})".html_safe
+    end
+
+    default_actions
+  end
 end
