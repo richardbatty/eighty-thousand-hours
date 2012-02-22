e18f520a0a0bdb7fde39d7573326c3b2bfe0dd8c
rjshade <robbie.shade@gmail.com>
2012-02-22 14:42:57 -0500

    different accordion states

--- app/assets/javascripts/accordion.js
+++ app/assets/javascripts/accordion.js
@@ -5,4 +5,9 @@ $(function() {
       autoHeight: false, 
       active: false
   });
+  $( ".accordion-expand-first" ).accordion({
+      collapsible: true, 
+      navigation: true,
+      autoHeight: false
+  });
 });
