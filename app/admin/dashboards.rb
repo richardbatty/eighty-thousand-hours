b2a841ebf2ed26b342251956ebf5e4f7595a9ab0
rjshade <robbie.shade@gmail.com>
2012-02-22 15:57:14 -0500

    check object exists in dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,11 +1,11 @@
 ActiveAdmin::Dashboards.build do
   section "Recently updated content" do
     table_for Version.order('id desc').limit(20) do
-      column "Edit" do |v| link_to "Edit", v.item.admin_permalink end
-      column "Title" do |v| v.item.respond_to?('title') ? v.item.title : v.item.name end
-      column "Type" do |v| v.item_type.underscore.humanize end
+      column "Edit" do |v|        v.item.respond_to?('admin_permalink') ? (link_to "Edit", v.item.admin_permalink) : "none" end
+      column "Title" do |v|       v.item.respond_to?('title') ? v.item.title : (v.item.respond_to?('name') ? v.item.name : "none") end
+      column "Type" do |v|        v.item_type.underscore.humanize end
       column "Modified at" do |v| v.created_at.to_s :long end
-      column "Edited by" do |v| (link_to User.find(v.whodunnit).name, admin_user_path(User.find(v.whodunnit))) if v.whodunnit end
+      column "Edited by" do |v|   (link_to User.find(v.whodunnit).name, admin_user_path(User.find(v.whodunnit))) if v.whodunnit end
     end
   end
 end
