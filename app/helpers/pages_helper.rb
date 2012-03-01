5d700beb80a17b17a9222d01262463d15e7dab55
rjshade <robbie.shade@gmail.com>
2012-03-01 17:51:15 -0500

    sidebar drawing

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -15,17 +15,26 @@ module PagesHelper
 
   # for each menu item (which is set to be displayed in the menu)
   # we recursively display its link and any children it has
-  def draw_menu( menu_items )
+  def draw_menu( menu_items, current_page = nil )
     result = ""
-    if menu_items
+    if menu_items.size > 0
       result += "\n<ul>\n"
       menu_items.display_in_menu.each do |c|
-        result += "<li>\n\t" \
-                  + c.get_menu_link \
-                  + draw_menu(c.children) \
-                  + "\n</li>\n"
+        active = (current_page == c)
+        result +=   "<li>" \
+                  + c.get_menu_link(active) \
+                  + draw_menu(c.children, current_page) \
+                  + "</li>\n"
       end
       result += "\n</ul>\n"
+    end
+    result.html_safe
+  end
+
+  def draw_sidebar_menu( current_page )
+    result = ""
+    if current_page
+      result += "<ul><li><a href='#'>blah</a></li></ul>"
     end
     result.html_safe
   end
