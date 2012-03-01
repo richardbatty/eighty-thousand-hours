f1aed0076694f0aae4c45158d080f52e386ac701
rjshade <robbie.shade@gmail.com>
2012-03-01 15:16:30 -0500

    editing new menu structure

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -12,5 +12,22 @@ module PagesHelper
       title
     end
   end
+
+  # for each menu item (which is set to be displayed in the menu)
+  # we recursively display its link and any children it has
+  def draw_menu( menu_items )
+    result = ""
+    if menu_items
+      result += "\n<ul>\n"
+      menu_items.display_in_menu.each do |c|
+        result += "<li>\n\t" \
+                  + c.get_menu_link \
+                  + draw_menu(c.children) \
+                  + "\n</li>\n"
+      end
+      result += "\n</ul>\n"
+    end
+    result.html_safe
+  end
 end
 
--- app/models/page.rb
+++ app/models/page.rb
@@ -4,10 +4,23 @@ class Page < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, :use => :slugged
 
+  belongs_to :parent,   :class_name => "Page"
+  has_many   :children, :class_name => "Page", :foreign_key => 'parent_id', :order => 'menu_priority DESC'
+
   # for versioning with paper_trail
   has_paper_trail
 
-  attr_accessible :title,:header_title,:body,:show_title,:show_box
+  attr_accessible :title,:header_title,:body,:show_box,:just_a_link,:menu_top_level,:menu_display,:menu_priority,:parent_id
+
+  scope :display_in_menu, where(:menu_display => true)
+  scope :top_level_menu, display_in_menu.where(:menu_top_level => true).order('menu_priority DESC')
+
+  def get_menu_link
+    # link that appears in menu structures.
+    # if page is 'just_a_link' then we want to link to whatever is stored in
+    # body field, otherwise we link to the page slug
+    "<a href='#{(just_a_link? ? body : '/'+slug)}'>#{title}</a>".html_safe
+  end
 
   # for active admin dashboard
   def admin_permalink
