31e061e29420dbdbd25b54acb2f0c1b9a82aa22e
rjshade <robbie.shade@gmail.com>
2012-03-02 11:41:45 -0500

    footer menu helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -15,18 +15,34 @@ module PagesHelper
 
   # for each menu item (which is set to be displayed in the menu)
   # we recursively display its link and any children it has
-  def draw_menu( menu_items, current_page = nil )
+  def draw_menu( menu_items, current_page = nil, outer_tag = "ul", inner_tag = "li" )
     result = ""
     if menu_items.size > 0
-      result += "\n<ul>\n"
+      result += "\n<#{outer_tag}>\n"
       menu_items.display_in_menu.each do |c|
         active = (current_page == c)
-        result +=   "<li>" \
+        result +=   "<#{inner_tag}>" \
                   + c.get_menu_link(active) \
-                  + draw_menu(c.children, current_page) \
-                  + "</li>\n"
+                  + draw_menu(c.children, current_page, outer_tag, inner_tag) \
+                  + "</#{inner_tag}>\n"
       end
-      result += "\n</ul>\n"
+      result += "\n</#{outer_tag}>\n"
+    end
+    result.html_safe
+  end
+
+  def draw_footer_menu
+    result = ""
+    menu_items = Page.top_level_menu_footer
+    if menu_items.size > 0
+      menu_items.display_in_menu_footer.each do |c|
+        result +=   "<ul>"\
+                  + "<li class='heading'>"+c.get_menu_link+"</li>"
+        c.children.display_in_menu_footer.each do |d|
+          result += "<li>"+d.get_menu_link+"</li>"
+        end
+        result += "</ul>\n"
+      end
     end
     result.html_safe
   end
