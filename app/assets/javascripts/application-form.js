9bcefe6e19a55e90e41905d2a53377eacec12464
rjshade <robbie.shade@gmail.com>
2012-03-07 23:31:50 -0500

    upped max HIC activity hours to 168

--- app/assets/javascripts/application-form.js
+++ app/assets/javascripts/application-form.js
@@ -29,6 +29,6 @@ function initialise_slider( name, range, min, max, step, start, display )
 
 $(document).ready(function() {
   initialise_slider("#user_eighty_thousand_hours_application_attributes_donation_percentage", "min", 10, 100, 5, 20);
-  initialise_slider("#user_eighty_thousand_hours_application_attributes_hic_activity_hours", "min", 0, 40, 5, 5);
+  initialise_slider("#user_eighty_thousand_hours_application_attributes_hic_activity_hours", "min", 0, 168, 5, 5);
   initialise_slider("#user_eighty_thousand_hours_application_attributes_average_income", "min", 10000, 5000000, 1000, 100000, function(x){ return x.formatMoney(0,'.',','); } );
 });
