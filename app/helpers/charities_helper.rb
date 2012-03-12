bcace27ca801e61a63b53b706efdc9418229f94a
rjshade <robbie.shade@gmail.com>
2012-03-12 19:28:12 +0000

    Removed old helper methods for donations

--- app/helpers/charities_helper.rb
+++
@@ -1,8 +0,0 @@
-module CharitiesHelper
-  def print_donation(d)
-    "
-    #{link_to d.user.name, user_path(d.user)} donated <strong>#{to_pounds(d.amount)}</strong>
-    on #{d.created_at.strftime "%B %d, %Y" }".html_safe
-  end
-end
-
--- app/helpers/members_helper.rb
+++
@@ -1,7 +0,0 @@
-module MembersHelper
-  def member_donation_full(d)
-    "<strong>#{to_pounds(d.amount)}</strong>
-    to #{link_to d.charity.name, charity_path(d.charity)}
-    on #{d.created_at.strftime "%B %d, %Y" }".html_safe
-  end
-end
