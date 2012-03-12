6964e4dc504aed72380142734e9af43b42adcfd0
rjshade <robbie.shade@gmail.com>
2012-03-12 10:50:40 +0000

    Newest endorsements first

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,6 +1,6 @@
 class EndorsementsController < ApplicationController
   def index
-    @endorsements = Endorsement.order("created_at ASC")
+    @endorsements = Endorsement.order("created_at DESC")
     @menu_current = 'Endorsements'
     @menu_root = 'About Us'
   end
