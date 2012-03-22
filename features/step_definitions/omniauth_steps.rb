be0e972cdcf7bdf97873f6c4464128267b611749
rjshade <robbie.shade@gmail.com>
2012-03-22 14:49:11 +0000

    Added pending step definition stubs for omniauth

---
+++ features/step_definitions/omniauth_steps.rb
@@ -0,0 +1,47 @@
+Given /^I am a logged in user$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I connect to Facebook$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I have a new authentication$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I can see the authentication on my account settings page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have an account$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have an authentication$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I am logged in$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^my Facebook email is the same as my account email$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I merge my account$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I am an unknown visitor$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I create an account$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I get an email$/ do
+  pending # express the regexp above with the code you wish you had
+end
