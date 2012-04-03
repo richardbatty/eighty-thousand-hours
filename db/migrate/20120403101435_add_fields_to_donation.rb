5e789027b9fdcd098efdb18ff892c7be09f0d245
rjshade <robbie.shade@gmail.com>
2012-04-03 11:49:28 +0100

    Migrations

--- db/migrate/20120403101435_add_fields_to_donation.rb
+++ db/migrate/20120403101435_add_fields_to_donation.rb
@@ -1,6 +1,6 @@
 class AddFieldsToDonation < ActiveRecord::Migration
   def change
     add_column :donations, :date, :date
-    add_column :donations, :currency, :string, :default => 'GBP'
+    add_column :donations, :currency, :string
   end
 end
