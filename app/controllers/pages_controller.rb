e4b681c1b1c70311df58dcc745c7deab4d539ec3
rjshade <robbie.shade@gmail.com>
2012-04-05 19:22:52 +0100

    Join form disabled if you've already applied/aren't logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,7 +29,7 @@ class PagesController < ApplicationController
       careers = ["banker","doctor","scientist","train_driver","campaigner","teacher"]#,"worker"]
       @careers = (careers.shuffle)[0..2]
 
-      @total_members = User.confirmed.size
+      @total_members = User.eighty_thousand_hours_members.size
 
       render :home
     end
