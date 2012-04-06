7bef55832f3a40a4c1663c205da5d1b26ec2f2f2
rjshade <robbie.shade@gmail.com>
2012-04-06 16:10:34 +0100

    to_s for symbols

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -11,12 +11,12 @@ class AuthenticationsController < ApplicationController
       sign_in_and_redirect(:user, auth.user)  
     elsif current_user
       current_user.authentications.find_or_create_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
-      flash[:"alert-success"] = "Authentication successful! You can now login using your #{omniauth['provider'].titleize} account."
+      flash[:"alert-success"] = "Authentication successful! You can now login using your #{omniauth['provider'].to_s.titleize} account."
       redirect_to edit_user_registration_path current_user
     elsif user = User.where( email: omniauth['info']['email'] ).first
       user.authentications.create(:provider => omniauth['provider'], :uid => omniauth['uid'])
       user.save!
-      flash[:"alert-success"] = "Your 80,000 Hours account is now linked to your #{omniauth['provider'].titleize} account, and you have been logged in."
+      flash[:"alert-success"] = "Your 80,000 Hours account is now linked to your #{omniauth['provider'].to_s.titleize} account, and you have been logged in."
       sign_in_and_redirect(:user, user)  
     else
       # store omniauth data in session
@@ -38,7 +38,7 @@ class AuthenticationsController < ApplicationController
 
       UserMailer.welcome_email(user).deliver!
 
-      flash[:"alert-success"] = "We've linked your #{omniauth['provider']} account!<br/>Your are signed in to 80,000 Hours with the name #{user.name}".html_safe
+      flash[:"alert-success"] = "We've linked your #{omniauth['provider'].to_s.titleize} account!<br/>Your are signed in to 80,000 Hours with the name #{user.name}".html_safe
       sign_in_and_redirect(:user, user) # devise helper method
     else
       redirect_to new_user_registration_path
