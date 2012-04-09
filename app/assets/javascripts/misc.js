2e44bccbec411a629e6948274ba2d641daddc272
rjshade <robbie.shade@gmail.com>
2012-04-09 19:21:55 +0100

    Loading state for form buttons

--- app/assets/javascripts/misc.js
+++ app/assets/javascripts/misc.js
@@ -2,39 +2,9 @@ $(document).ready(function () {
   // smooth scrolling on all local links
   $.localScroll();
 
-
-  // default text in form inputs
-  $(".defaultText").focus(function(srcc)
-    {
-      if ($(this).val() == $(this)[0].title)
-  {
-    $(this).removeClass("defaultTextActive");
-    $(this).val("");
-  }
-    });
-
-  $(".defaultText").blur(function()
-    {
-      if ($(this).val() == "")
-  {
-    $(this).addClass("defaultTextActive");
-    $(this).val($(this)[0].title);
-  }
-    });
-
-  $(".defaultText").blur();     
+  // enable button loading state on all form buttons
+  $('form').find('.btn').button()
+  $('form').find('.btn').click(function() {
+    $(this).button('loading');
+  });
 });
-
-// for the sticky sidebar on the blog pages
-// pretty hacky with fixed left offset...
-//window.onscroll = function () {
-//  if ($('body').scrollTop() >= 223)
-//  {
-//    $('#sidebar').css({ position: "fixed", left: 872, top: 0 })
-//  }
-//  else
-//  {
-//    $('#sidebar').removeAttr("style")
-//  }
-//}
-
