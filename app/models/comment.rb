f658fd3e0df34b20273dd59e25fcbaddfce9c3ac
rjshade <robbie.shade@gmail.com>
2012-04-13 18:30:09 +0100

    Model relations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ActiveRecord::Base
   validates_presence_of :body,    message: "Can't be blank"
   validates_presence_of :post_id, message: "Comment must be on a specific post"
   validates_presence_of :user_id, message: "Comment must be by a specific user"
+
+  belongs_to :user
+  belongs_to :post
 end
