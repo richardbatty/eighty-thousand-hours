435abc730d0b579fd62c8fad4bebbcec7f45fbde
rjshade <robbie.shade@gmail.com>
2012-04-13 18:24:40 +0100

    Comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ActiveRecord::Base
+  validates_presence_of :body,    message: "Can't be blank"
+  validates_presence_of :post_id, message: "Comment must be on a specific post"
+  validates_presence_of :user_id, message: "Comment must be by a specific user"
 end
