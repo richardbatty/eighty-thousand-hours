856799f2abc8fe253ba79f30242355a1184113fd
rjshade <robbie.shade@gmail.com>
2012-04-14 23:05:01 +0100

    Comments admin page

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -1,5 +1,4 @@
 ActiveAdmin.register Comment, :as => "PostComment" do
-  menu :if => proc{ can?(:manage, Comment) },
-       :label => "Comments"
-  controller.authorize_resource
+  #menu :if => proc{ can?(:manage, Post) }
+  #controller.authorize_resource
 end
