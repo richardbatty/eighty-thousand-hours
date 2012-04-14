1ddff1a484dfd5c7ec0a0ea11e7e88a6a3f73198
rjshade <robbie.shade@gmail.com>
2012-04-14 19:17:33 +0100

    Single threaded comments working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,6 @@
 class CommentsController < ApplicationController
   load_and_authorize_resource
 
-  def new
-    @comment = Comment.new(parent_id: params[:parent_id], post_id: params[:post_id])
-  end
-
   def create
     @comment = Comment.create( params[:comment] )
     @comment.user = current_user
