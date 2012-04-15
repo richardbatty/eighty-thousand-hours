ccdb8ab04f60b5fec41089db6afd3c77608bb33e
rjshade <robbie.shade@gmail.com>
2012-04-15 14:06:06 +0100

    Correctly weighting endorsements

--- app/models/endorsement.rb
+++ app/models/endorsement.rb
@@ -4,6 +4,13 @@ class Endorsement < ActiveRecord::Base
   def self.get_header_endorsement
     # an endorsement with a higher weight is more likely
     # to appear in the header...
-    where(header: true).order('random()*weight').last
+    where(header: true).order('random()/weight').first
+  end
+
+  before_save :ensure_positive_weight
+
+  private
+  def ensure_positive_weight
+    self.weight = 1 if (self.weight < 1)
   end
 end
