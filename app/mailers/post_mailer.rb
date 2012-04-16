ed835a8a5cfb1395486cedf5cf2f2c32020138fb
rjshade <robbie.shade@gmail.com>
2012-04-16 08:42:31 +0100

    Fixes to post comment mailer

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -2,10 +2,15 @@ class PostMailer < ActionMailer::Base
   default from: "admin@80000hours.org"
 
   def new_comment(comment)
-    @comment = comment
-    if @comment.post.user
-      @name = comment.post.user.first_name
-      mail to: [comment.post.user.email, 'admin@80000hours.org'], subject: "[80,000 Hours] New comment on #{comment.post.title}"
+    # only try and send an email if a User owns the post
+    if comment.post.user
+      @post_author = comment.post.user.name
+      @post_author_email = comment.post.user.email
+
+      @comment = comment
+      @commenter_name = (@comment.user ? comment.user.first_name : comment.name)
+
+      mail to: [@post_author_email, 'admin@80000hours.org'], subject: "[80,000 Hours] New comment on #{comment.post.title}"
     end
   end
 end
