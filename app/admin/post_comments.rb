ff4732676d2443de5c64d25bf6ba449bc112bfca
rjshade <robbie.shade@gmail.com>
2012-04-17 11:02:15 +0100

    Better comment admin page

--- app/admin/post_comments.rb
+++ app/admin/post_comments.rb
@@ -2,4 +2,14 @@ ActiveAdmin.register Comment, :as => "PostComment" do
   menu :if => proc{ can?(:manage, Post) },
        :label => "Comments",
        :parent => "Blog"
+
+  index do
+    column :id
+    column :name
+    column :email
+    column :user
+    column :body
+    column :created_at
+    default_actions
+  end
 end
