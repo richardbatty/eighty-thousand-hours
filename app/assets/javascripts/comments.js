c86212a152b0fc572dbbdf50a97366f8c591c003
rjshade <robbie.shade@gmail.com>
2012-04-22 21:54:27 +0100

    Reply to comment links

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -3,4 +3,10 @@ $(document).ready( function() {
     $('input[type=submit]', this).attr('disabled', 'disabled');
     //$('input[type=submit]', this).val('Posting...');
   });
+
+  $('a.reply').bind('click', function(event) {
+    var replytext = $(this).siblings('.replytext').html();
+    $('#comment-body').focus();
+    $('#comment-body').val(replytext + "\n\n----\n\n")
+  });
 });
