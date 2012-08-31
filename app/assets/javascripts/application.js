b3a85684e5bea1f239fb5a4abc46320c9e11b1b9
rjshade <robbie.shade@gmail.com>
2012-08-31 14:20:42 +0100

    Removed smooth scrolling JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,8 +2,6 @@
 //= require jquery-ui-1.8.17.custom.min
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require jquery.localscroll-1.2.7-min
-//= require jquery.scrollTo-1.4.2-min
 //= require jquery.easing
 //= require fancybox
 //= require highcharts
--- app/assets/javascripts/misc.js
+++ app/assets/javascripts/misc.js
@@ -1,7 +1,4 @@
 $(document).ready(function () {  
-  // smooth scrolling on all local links
-  $.localScroll();
-
   // enable button loading state on all form buttons
   //$('form').find('.btn').button()
   //$('form').find('.btn').click(function() {
