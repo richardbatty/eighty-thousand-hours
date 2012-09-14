7dee510c03530a72ffe174e36fc3fd8cd34025b6
rjshade <robbie.shade@gmail.com>
2012-09-14 14:07:07 +0100

    Loading state for button

--- app/assets/javascripts/misc.js
+++ app/assets/javascripts/misc.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {  
   // enable button loading state on all form buttons
-  //$('form').find('.btn').button()
-  //$('form').find('.btn').click(function() {
-  //  $(this).button('loading');
-  //});
+  $('form').find('.btn').button()
+  $('form').find('.btn').click(function() {
+    $(this).button('loading');
+  });
   $('.toggleLink').click(function(){
     $(this).hide();
     $(this).siblings('.toggle_div').show();
