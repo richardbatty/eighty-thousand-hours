a0e0ee8459d5f9b71f13aa6d1471c0a0f93542d8
rjshade <robbie.shade@gmail.com>
2012-09-19 20:43:56 +0100

    Comment email notifications for authors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,22 +3,14 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new( params[:comment] )
-    #if params[:comment][:blog_post_id].nil?
-      #@comment.discussion_post = DiscussionPost.find(params[:comment][:discussion_post_id])
-    #else
-      #@comment.blog_post = BlogPost.find(params[:comment][:blog_post_id])
-    #end
-
     @comment.user = current_user if current_user
+
     if @comment.save
-      # * Only try and send an email if a User owns the post
-      # * Don't email if author commenting on their own post
-      # * Only do this is a blog post
-      #if !params[:comment][:blog_post_id].nil?
-        #if @comment.blog_post.user && (@comment.blog_post.user != @comment.user)
-          #BlogPostMailer.new_comment(@comment).deliver!
-        #end
-      #end
+      if @comment.blog_post
+        BlogPostMailer.new_comment(@comment).deliver!
+      else
+        DiscussionPostMailer.new_comment(@comment).deliver!
+      end
 
       render 'comments/create'
     else
