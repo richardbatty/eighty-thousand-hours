087d89fc7fdeb409a31e1f11764fa775948993c5
Richard Batty <rjlbatty@gmail.com>
2012-10-22 14:27:49 +0100

    reversed title text so '80000 hours' comes last

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,6 @@
 module PagesHelper
   def title
-    "80,000 Hours" + ( (@title.to_s == '') ? "" : " | #{@title}" )
+     ( (@title.to_s == '') ? "" : "#{@title}" ) + " | 80,000 Hours"
   end
 
   def page_title(tag=true)
