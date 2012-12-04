39ecf1aa64ee2e38df05c6b1e2b1e87cafd49858
rjshade <robbie.shade@gmail.com>
2012-12-03 20:19:59 -0500

    Improved CSV output of EtkhProfiles

--- app/admin/etkh_profiles.rb
+++ app/admin/etkh_profiles.rb
@@ -38,4 +38,36 @@ ActiveAdmin.register EtkhProfile do
       row :donation_percentage_optout
     end
   end
+
+  csv do
+    # These first few fields refer to the User that owns this EtkhProfile
+    column ("Name")     { |profile| profile.user.name }
+    column ("Real name"){ |profile| profile.user.real_name }
+    column ("Email")    { |profile| profile.user.email }
+    column ("Location") { |profile| profile.user.location }
+
+    # The remaining fields are in the EtkhProfile model
+    column :background
+    column :organisation
+    column :public_profile
+    column :skills_knowledge_share
+    column :skills_knowledge_learn
+    column :causes_comment
+    column :activities_comment
+    column :donation_percentage
+    column :donation_percentage_optout
+
+    # The causes/activities need special treatment as they live
+    # in a separate database table, and we loop over each possible
+    # cause, printing out ones that this EtkhProfile has selected
+    column :profile_option_causes do |p|
+      p.profile_option_causes.map{|c| c.title}.join(', ')
+    end
+    column :causes_comment
+
+    column :profile_option_activities do |p|
+      p.profile_option_activities.map{|c| c.title}.join(', ')
+    end
+    column :activities_comment
+  end
 end
