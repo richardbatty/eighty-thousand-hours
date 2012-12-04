bb49e277eeb1471289de06aab3d818826e706eb4
rjshade <robbie.shade@gmail.com>
2012-12-03 22:22:55 -0500

    Logout needs GET?

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -66,7 +66,7 @@ ActiveAdmin.setup do |config|
   # link. For example :get, :delete, :put, etc..
   #
   # Default:
-  config.logout_link_method = :delete
+  config.logout_link_method = :get
   
   # == Admin Comments
   #
